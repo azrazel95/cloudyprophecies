58a529ec37b1fe0a832c19b15ef2e1fb413c2299
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-03-09 15:54:57 -0500

    everything but the recentsearch works

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,113 @@
 var searchLoc = $("#searchLocation");
+// console.log(searchLoc.val());
+// let searchItem = searchLoc.val();
+// console.log(searchItem)
 var name = $("#name");
 var date = $("#date");
 var temp = $("#temp");
 var wind = $("#windspeed");
 var hum = $("#humidity");
 var btn = $("#searchbtn");
-
-
+var recentSearchList = $("#recentSearchList")
 var currentWeatherTitle = $(`#currentWeatherTitle`);
 var currentTemp = $(`#currentTemperature`);
 var currentHum = $(`#currentHum`);
 var currentWind = $(`#currentWind`);
+var day0 = new Array(
+    day0Title = $("#day0Title"),
+    day0Icon = $("#day0Icon"),
+    day0Temp = $("#day0Temperature"),
+    day0Hum = $("#day0Humidity"),
+    day0Wind = $("#day0WindSpeed"),
+);
+var day1 = new Array(
+    day1Title = $("#day1Title"),
+    day1Icon = $("#day1Icon"),
+    day1Temp = $("#day1Temperature"),
+    day1Hum = $("#day1Humidity"),
+    day1Wind = $("#day1WindSpeed"),
+);
+var day2 = new Array(
+    day2Title = $("#day2Title"),
+    day2Icon = $("#day2Icon"),
+    day2Temp = $("#day2Temperature"),
+    day2Hum = $("#day2Humidity"),
+    day2Wind = $("#day2WindSpeed"), 
+);
+var day3 = new Array(
+    day3Title = $("#day3Title"),
+    day3Icon = $("#day3Icon"),
+    day3Temp = $("#day3Temperature"),
+    day3Hum = $("#day3Humidity"),
+    day3Wind = $("#day3WindSpeed"),
+);
+var day4 = new Array(
+    day4Title = $("#day4Title"),
+    day4Icon = $("#day4Icon"),
+    day4Temp = $("#day4Temperature"),
+    day4Hum = $("#day4Humidity"),
+    day4Wind = $("#day4WindSpeed"),
+);
 
+var fiveDaycards = new Array(
+    day0,
+    day1,
+    day2,
+    day3,
+    day4
+)
+var search = [];
+function getLocalStorage(){
+    
+// ...
+if (localStorage.getItem('search') !== null) {
+    search = JSON.parse(localStorage.getItem('search'));
+} else {
+    localStorage.setItem("search", JSON.stringify(search));
+}
+}
+
+
+function renderRecentSearch(){
+    recentSearchList.empty()
+    getLocalStorage()
+    
+    for (let i = 0; i<search.length; i++){
+        
+        
+        var recentSearch = search[i];
+        var li = document.createElement("li");
+        li.textContent = recentSearch;
+        li.setAttribute("class", "list-group-item"+" pt-1"+" pl-2"+" bg-danger"+" ml-2"+" data-location"+i);
+        li.id = "search" + [i]
+        recentSearchList.prepend(li)
+        li.addEventListener("click",function(event){
+            
+                var searchLoc = li.textContent
+                apiGetter();
+            
+        })
+        
+    }
+}
 
 
 btn.on("click", apiGetter)
 function apiGetter(event){
     event.preventDefault();
-    var api = "https://api.openweathermap.org/data/2.5/weather?q=" + searchLoc.val() + "&appid=78e1806e46a60decb6a154f57a6b79ea&&units=imperial"
+    let city = searchLoc.val()
+
+    function storeSearch(){
+        if(!search.some(item=> item.toLowerCase() === city.toLowerCase())){
+            search.push(city)
+            searchLoc.val("")
+            localStorage.setItem("search", JSON.stringify(search));
+        }
+    
+    }
+    var api = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=78e1806e46a60decb6a154f57a6b79ea&&units=imperial"
     fetch(api)
 .then(function(response){
-    
     return response.json();
 })
 .then(function(data){
@@ -32,56 +119,11 @@ function apiGetter(event){
     var lon = data.coord.lon;
     var lat = data.coord.lat;
     var fiveDay = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=78e1806e46a60decb6a154f57a6b79ea&&units=imperial`;
-    var day0 = new Array(
-        day0Title = $("#day0Title"),
-        day0Icon = $("#day0Icon"),
-        day0Temp = $("#day0Temperature"),
-        day0Hum = $("#day0Humidity"),
-        day0Wind = $("#day0WindSpeed"),
-    );
-    var day1 = new Array(
-        day1Title = $("#day1Title"),
-        day1Icon = $("#day1Icon"),
-        day1Temp = $("#day1Temperature"),
-        day1Hum = $("#day1Humidity"),
-        day1Wind = $("#day1WindSpeed"),
-    );
-    var day2 = new Array(
-        day2Title = $("#day2Title"),
-        day2Icon = $("#day2Icon"),
-        day2Temp = $("#day2Temperature"),
-        day2Hum = $("#day2Humidity"),
-        day2Wind = $("#day2WindSpeed"), 
-    );
-    var day3 = new Array(
-        day3Title = $("#day3Title"),
-        day3Icon = $("#day3Icon"),
-        day3Temp = $("#day3Temperature"),
-        day3Hum = $("#day3Humidity"),
-        day3Wind = $("#day3WindSpeed"),
-    );
-    var day4 = new Array(
-        day4Title = $("#day4Title"),
-        day4Icon = $("#day4Icon"),
-        day4Temp = $("#day4Temperature"),
-        day4Hum = $("#day4Humidity"),
-        day4Wind = $("#day4WindSpeed"),
-    );
-
-    var fiveDaycards = new Array(
-        day0,
-        day1,
-        day2,
-        day3,
-        day4
-    )
-
     fetch(fiveDay)
     .then(function (response){
         return response.json();
     })
     .then(function(forecastData){
-        console.log("data", forecastData)
         var dailyList = new Array(
             first = $(forecastData.list[5]),
             second = $(forecastData.list[13]),
@@ -89,14 +131,7 @@ function apiGetter(event){
             fourth = $(forecastData.list[29]),
             fifth = $(forecastData.list[37]),
         )
-        console.log("daily", dailyList)
-        console.log("obj", fiveDaycards)
         for(var i=0; i<fiveDaycards.length; i++){
-            console.log(dailyList[i]);
-            
-            
-            for (var j=0; j<i; j++)
-            console.log(i)
             fiveDaycards[i][0].html(`${forecastData.city.name}${moment(dailyList[i][0].dt, "X").format("(MM/DD/YYYY)")}`);
             fiveDaycards[i][1].html(`<img src="http://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}@2x.png">`);
             fiveDaycards[i][2].html(`Temp: ${dailyList[i][0].main.temp}F°`)
@@ -106,5 +141,8 @@ function apiGetter(event){
     })
     
 })
+storeSearch();
+renderRecentSearch()
 }
-
+getLocalStorage();
+renderRecentSearch();
